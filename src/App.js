1accd8ae496ca149064b9d2cd4793995fed9e8da
devilar <devilar.sd@gmail.com>
2021-03-05 18:03:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useEffect,useState} from 'react';
 
 function App() {
+
+const [gallery,setGallery] = useState([])
+
+useEffect(()=>{
+
+fetch('https://boiling-refuge-66454.herokuapp.com/images')
+    .then(response=>response.json())
+    .then(json=>console.log('yalla',json))
+},[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Beatefull photogallery</h1>
     </div>
   );
 }
