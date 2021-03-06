6147b5e1b975e18a22617b3d77fc07e43f7bfb4a
devilar <devilar.sd@gmail.com>
2021-03-06 12:13:30 +0300

    final

--- src/Modal/Modal.jsx
+++ src/Modal/Modal.jsx
@@ -9,30 +9,21 @@ const Modal = ({active,setActive,content}) =>{
 
     function submitHandler(event,id){
         event.preventDefault()
-
-        console.log('content id is', id)
-
         fetch(`https://boiling-refuge-66454.herokuapp.com/images/237/comments`,{
             method: "POST",
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json',
             },
-            status: 204,
             body:  JSON.stringify({
                 name:'fake name',
                 comment:'fake comment'
 
             }),
         }
-
         )
             .then(response => response.json())
             .then(json=>console.log('Тут хочу получить json, но ничего не приходит',json))
-
-
-
-
     }
 
     const[value,setValue] = useState('')
